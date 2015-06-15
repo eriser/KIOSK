8d74a972a05089c1c936ed3fb74b3a4c4fa864fe
tale <giangnguyen.tale@gmail.com>
2015-06-15 22:32:16 +0700

    + handle power button and system dialog show

--- app/src/main/java/com/tale/homeit/MainActivity.java
+++ app/src/main/java/com/tale/homeit/MainActivity.java
@@ -1,7 +1,9 @@
 package com.tale.homeit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.WindowManager;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,8 +14,36 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * Not exit if home pressed.
+     */
     @Override
     public void onBackPressed() {
         // Here we do nothing.
     }
+
+    /**
+     * Handle long press POWER button.
+     */
+    @Override
+    public void onAttachedToWindow() {
+        getWindow().addFlags(
+                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+        getWindow().addFlags(
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+    }
+
+    /**
+     * Handle system dialog showing
+     */
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (!hasFocus) {
+            Intent closeDialog =
+                    new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+            sendBroadcast(closeDialog);
+        }
+    }
+
 }
